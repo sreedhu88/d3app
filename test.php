bc17c41eaa9d06adb2cf2c03c25133fecdd3d925
sreedhu88 <sreedhu_krishnan@infosys.com>
2018-12-10 13:11:46 +0530

    update test

--- test.php
+++ test.php
@@ -77,7 +77,9 @@ echo"
 
 <form method='post' enctype='multipart/form-data'>";
 
-
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
 echo"<br>
 <h4 align=center>Name the document with your employee id before you upload. eg. 152245.docx</h4>
 <table align=center border=1>
@@ -88,6 +90,9 @@ echo"<br>
 
 <tr align=center><td colspan=2 style=text-align:center><input type='submit' value='Upload Idea' name='submit' ></tr>
 </table>
+</div>
+  </div>
+</div>
 </form>
 <br>
 
